abd8cff9da611fc8927982aeda61da9ab57fae50
SIY1121 <sota.0512.0213@gmail.com>
2021-02-21 09:07:50 +0000

    update: proto-gen

--- proto-gen.sh
+++ proto-gen.sh
@@ -7,22 +7,41 @@ mkdir -p ./generated/services/timetable
 
 ### course
 yarn pbjs \
-    --target static-module \
-    --out generated/services/course/index.js \
-    ./services/course-service/protos/CourseService.proto
+  --target static-module \
+  --no-encode \
+  --no-decode \
+  --path ../../ \
+  --out generated/services/course/index.js \
+  ./services/course-service/protos/CourseService.proto
 
 yarn pbts \
-    --out ./generated/services/course/index.d.ts \
-    ./generated/services/course/index.js
+  --out ./generated/services/course/index.d.ts \
+  ./generated/services/course/index.js
+
+
+### https://github.com/protobufjs/protobuf.js/issues/1222
+sed -i -e "s/\[ 'Promise' \]\./Promise/g" "generated/services/course/index.d.ts"
+sed -i -e "s/\[ 'object' \]\.<string, any>/{ [k: string]: any }/g" "generated/services/course/index.d.ts"
+sed -i -e "s/\[ 'Array' \]\./Array/g" "generated/services/course/index.d.ts"
+###
 
 ### timetable
 yarn pbjs \
-    --target static-module \
-    --out generated/services/timetable/index.js \
-    ./services/timetable-service/protos/Nullable.proto \
-    ./services/timetable-service/protos/Message.proto \
-    ./services/timetable-service/protos/TimetableService.proto
+  --target static-module \
+  --no-encode \
+  --no-decode \
+  --path ../../ \
+  --out generated/services/timetable/index.js \
+  ./services/timetable-service/protos/Nullable.proto \
+  ./services/timetable-service/protos/Message.proto \
+  ./services/timetable-service/protos/TimetableService.proto
 
 yarn pbts \
-    --out ./generated/services/timetable/index.d.ts \
-    ./generated/services/timetable/index.js
+  --out ./generated/services/timetable/index.d.ts \
+  ./generated/services/timetable/index.js
+
+### https://github.com/protobufjs/protobuf.js/issues/1222
+sed -i -e "s/\[ 'Promise' \]\./Promise/g" "generated/services/timetable/index.d.ts"
+sed -i -e "s/\[ 'object' \]\.<string, any>/{ [k: string]: any }/g" "generated/services/timetable/index.d.ts"
+sed -i -e "s/\[ 'Array' \]\./Array/g" "generated/services/timetable/index.d.ts"
+###
