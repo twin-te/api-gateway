64221255d93beb8c171e8034bb20f158af062e25
SIY1121 <sota.0512.0213@gmail.com>
2021-03-13 12:45:07 +0000

    add: getTimetableByDate

--- src/gateway/schoolCalendarService/index.ts
+++ src/gateway/schoolCalendarService/index.ts
@@ -1,3 +1,4 @@
+import { Dayjs } from 'dayjs'
 import { IEvent, IModuleTerm } from '../../../generated/services/schoolCalendar'
 import { All } from '../../type/utils'
 import { schoolCalendarServiceClient } from '../grpc'
@@ -10,11 +11,31 @@ export const schoolCalendarService = {
         else resolve(res.events as All<IEvent>[])
       })
     }),
+  getEventsByDate: (date: Dayjs) =>
+    new Promise<All<IEvent>[]>((resolve, reject) => {
+      schoolCalendarServiceClient.getEventsByDate(
+        { date: date.toISOString() },
+        (err, res) => {
+          if (err || !res) reject(err)
+          else resolve(res.events as All<IEvent>[])
+        }
+      )
+    }),
   getModuleTerms: (year: number) =>
     new Promise<All<IModuleTerm>[]>((resolve, reject) => {
       schoolCalendarServiceClient.getModuleTerms({ year }, (err, res) => {
         if (err || !res) reject(err)
         else resolve(res.terms as All<IModuleTerm>[])
       })
+    }),
+  getModuleTermByDate: (date: Dayjs) =>
+    new Promise<All<IModuleTerm>>((resolve, reject) => {
+      schoolCalendarServiceClient.getModuleTermByDate(
+        { date: date.toISOString() },
+        (err, res) => {
+          if (err || !res) reject(err)
+          else resolve(res as All<IModuleTerm>)
+        }
+      )
     }),
 }
---
+++ src/usecase/getTimetableByDate.ts
@@ -0,0 +1,29 @@
+import { Dayjs } from 'dayjs'
+import { schoolCalendarService } from '../gateway/schoolCalendarService'
+import { CourseSchedule, Day, Module } from '../type/course'
+import { getRegisteredCourses } from './getRegisteredCourse'
+
+export async function getTimetableByDate(userId: string, date: Dayjs) {
+  const nendo = date.month() < 4 ? date.year() - 1 : date.year()
+
+  const [courses, module, events] = await Promise.all([
+    getRegisteredCourses(userId, nendo),
+    schoolCalendarService.getModuleTermByDate(date),
+    schoolCalendarService.getEventsByDate(date),
+  ])
+
+  const day = date.day() as Day
+
+  return {
+    courses: courses.filter((c) => {
+      const schedules = (c.schedules ?? c.course?.schedules) as CourseSchedule[]
+      return schedules.find(
+        (s) =>
+          (s.module === module.module || s.module === Module.Annual) &&
+          s.day === day
+      )
+    }),
+    module: module.module,
+    events,
+  }
+}
